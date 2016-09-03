be08a6503e523fc402ea94a3efc460bf24e8a9b6
Midova <darya.demidova2011@gmail.com>
2016-09-03 11:43:44 +0300

    Создала класс "транзакция"

---
+++ TransactionLibrary/TransactionLibrary/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionLibrary
+{
+	class Transaction
+	{
+		public double Amount { get; private set; }
+
+		public string Categry { get; private set; }
+	}
+}
