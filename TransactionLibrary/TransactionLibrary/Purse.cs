f8ee08d8178b7249cf19e43007b92f14f5d8bb48
Midova <darya.demidova2011@gmail.com>
2016-09-14 16:13:08 +0300

    Набросала фронт работы. Свойство баланс и три метода-фильтра

--- TransactionLibrary/TransactionLibrary/Purse.cs
+++ TransactionLibrary/TransactionLibrary/Purse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,64 @@ namespace TransactionLibrary
 {
 	class Purse
 	{
+		
+		public Purse()
+		{
+			NamePursr = "";
+			PerfectTransaction = new ObservableCollection<TransactionPerfect>();
+			TemplateTransaction = new ObservableCollection<TransactionTemplate>();
+			FutureTransaction = new ObservableCollection<TransactionFuture>();
+		}
+		
+		public Purse (string namePurse)
+		{
+			NamePursr = namePurse;
+			PerfectTransaction = new ObservableCollection<TransactionPerfect>();
+			TemplateTransaction = new ObservableCollection<TransactionTemplate>();
+			FutureTransaction = new ObservableCollection<TransactionFuture>();
+		}
+		
+		/// <summary>
+		/// название кошелька
+		/// </summary>
+		public string NamePursr { get; private set; }
+
+		/// <summary>
+		/// Список совершенных операций
+		/// </summary>
+		public ObservableCollection<TransactionPerfect> PerfectTransaction { get; set; }
+
+		/// <summary>
+		/// Список шаблонов транзакций
+		/// </summary>
+		public ObservableCollection<TransactionTemplate> TemplateTransaction { get; set; }
+
+		/// <summary>
+		/// список планируемых операций
+		/// </summary>
+		public ObservableCollection<TransactionFuture> FutureTransaction { get; set; }
+
+		public double GetBalance
+		{
+			get
+			{
+				
+			}
+		}
+
+		public double GetIncomePeriod(DateTime startDate, DateTime endDate)
+		{
+
+		}
+
+		public double GetCostPeriod(DateTime startDate, DateTime endDate)
+		{
+
+		}
+
+		public IEnumerable<TransactionPerfect> GetTransactionPeriod(DateTime startDate, DateTime endDate)
+		{
+
+		}
 	}
 }
